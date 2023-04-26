cc72977d2999a8873373b0ac7977ded374f3e758
Nicholas Cabral <nicholasscabral@gmail.com>
2023-04-26 11:38:19 -0300

    feat: ass constants, https status and erros messages

---
+++ src/errors/messages.ts
@@ -0,0 +1,3 @@
+export const ERROR_MISSING_FIELDS = 'Fields are missing';
+export const ERROR_PASSWORDS_DONT_MATCH = 'Passwords do not match';
+export const ERROR_EMAIL_IN_USE = 'Email already in use';
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_HASH_ROUNDS = 8;
+export const DEFAULT_PORT = 3001;
